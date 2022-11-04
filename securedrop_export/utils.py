115209e1472f9b016d7388a27c527b8a9be4910a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-04 10:48:42 +0000

    Adding tarfile member sanitization to extractall()

--- securedrop_export/utils.py
+++ securedrop_export/utils.py
@@ -77,7 +77,26 @@ def safe_extractall(archive_file_path: str, dest_path: str) -> None:
                 if Path(file_info.linkname).is_absolute():
                     relative_filepath(file_info.linkname, dest_path)
 
-        tar.extractall(dest_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, dest_path)
 
 
 def relative_filepath(filepath: Union[str, Path], base_dir: Union[str, Path]) -> Path:
